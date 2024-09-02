037b383ac755d9492379a2a3a6b35c300c52e567
Aaryan Nagpal <aaryannagpal@gmail.com>
2024-09-02 19:40:54 +0530

    Create a0.cpp

---
+++ a0.cpp
@@ -0,0 +1,10 @@
+// this is a sample template file, in C++
+// All this file does is print out the result mentioned between '<<' and '>>'
+#include <iostream>
+
+int main() {
+    std::cout << "..." << std::endl;
+    // Instead of '...', write "Hello World" within quotes, something like:
+    // std::cout << "Hello World" << std::endl;
+    return 0;
+}
